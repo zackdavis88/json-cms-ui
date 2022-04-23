9c39b22118831628a2db13ab98d2f6049250fcb8
Zack Davis <zack.davis88@hotmail.com>
2022-04-23 16:26:04 -0700

    making config folder into a module

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
+const { API_FQDN } = require('./config/app');
 const withTM = require('next-transpile-modules')(['@mui/material']);
 module.exports = withTM({
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:3333/:path*',
+        destination: `${API_FQDN}/:path*`,
       },
     ];
   },
