b6a5aef9267a61d1094ccc0de32d4f3373d609ea
Zack Davis <zack.davis88@hotmail.com>
2022-05-01 08:30:19 -0700

    refactoring Navbar code more

--- src/modules/Navbar/components/index.ts
+++ src/modules/Navbar/components/index.ts
@@ -1,4 +1,3 @@
 export { default as Brand } from './Brand';
 export { default as NavigationButtons } from './NavigationButtons';
-export { default as ContentFlexBox } from './ContentFlexBox';
 export { default as NavigationSidebar } from './NavigationSidebar';
--- src/modules/Navbar/layout/NavbarLayout.tsx
+++ src/modules/Navbar/layout/NavbarLayout.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import { AppBar, Container, Toolbar } from '@mui/material';
+import { AppBar, Container, Toolbar, Box } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
 interface NavbarLayoutProps {
   children: React.ReactNode;
 }
 
 const NavbarLayout = ({ children }: NavbarLayoutProps) => {
+  const theme = useTheme();
   return (
     <AppBar position="sticky">
       <Container maxWidth="xl">
-        <Toolbar disableGutters>{children}</Toolbar>
+        <Toolbar disableGutters>
+          <Box
+            display="flex"
+            width="100%"
+            height={theme.mixins.toolbar.height}
+            flexDirection="row"
+            justifyContent="space-between"
+          >
+            {children}
+          </Box>
+        </Toolbar>
       </Container>
     </AppBar>
   );
