5449b37ff3daa86f1e351f4bef077e4d7372f413
Zack Davis <zack.davis88@hotmail.com>
2022-05-14 18:34:13 -0700

    implemented Notification functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
-import { Container } from '@mui/material';
+import { Container, Box } from '@mui/material';
 import { Provider } from 'react-redux';
 import { theme } from 'src/theme';
 import { useStore } from 'src/hooks';
-import { Navbar } from 'src/modules';
+import { Navbar, Notification } from 'src/modules';
 
 export default function App({ Component, pageProps }: AppProps) {
   const store = useStore(pageProps.initialReduxState);
@@ -15,14 +15,17 @@ export default function App({ Component, pageProps }: AppProps) {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Navbar />
-        <Container maxWidth="xl" disableGutters>
-          {/* TODO: Wrap <Component> within some sort of <Auth> component when authRequired is true */}
-          {pageProps.authRequired ? (
-            <Component {...pageProps} />
-          ) : (
-            <Component {...pageProps} />
-          )}
-        </Container>
+        <Box width="100vw" position="relative">
+          <Container maxWidth="xl" disableGutters>
+            <Notification />
+            {/* TODO: Wrap <Component> within some sort of <Auth> component when authRequired is true */}
+            {pageProps.authRequired ? (
+              <Component {...pageProps} />
+            ) : (
+              <Component {...pageProps} />
+            )}
+          </Container>
+        </Box>
       </ThemeProvider>
     </Provider>
   );
--- src/modules/SignUpForm/SignUpForm.tsx
+++ src/modules/SignUpForm/SignUpForm.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ActionButtons, NewUserInputs } from './components';
 import { SignUpFormLayout } from './layout';
-import { useDispatchCreateUser } from 'src/hooks';
+import { useDispatchCreateUser, useDispatchShowNotification } from 'src/hooks';
 import { useUserLoading } from './hooks';
 
 interface FormInputState {
@@ -28,7 +28,7 @@ const SignUpForm = ({ onBackClick }: SignUpFormProps) => {
   });
   const requestLoading = useUserLoading();
   const createUser = useDispatchCreateUser();
-
+  const showNotification = useDispatchShowNotification();
   const hasNoInputValues = !(username.value && password.value && confirmPassword.value);
   const hasInputError = !!(username.error || password.error || confirmPassword.error);
   const submitDisabled = hasNoInputValues || hasInputError || requestLoading;
@@ -53,7 +53,9 @@ const SignUpForm = ({ onBackClick }: SignUpFormProps) => {
       }
     }
 
-    // TODO: Set a success notification once notifications are a thing...
+    if (body.message) {
+      showNotification(body.message, 'success');
+    }
     onBackClick();
   };
 
