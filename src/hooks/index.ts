88d9ab7e2751ac52efdd48ea3d10f5b1cb295733
Zack Davis <zack.davis88@hotmail.com>
2022-05-24 18:13:37 -0700

    making useUserLoading a reusable hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -5,3 +5,4 @@ export { default as useDispatchSignOut } from './useDispatchSignOut';
 export { default as useDispatchCreateUser } from './useDispatchCreateUser';
 export { default as useDispatchShowNotification } from './useDispatchShowNotification';
 export { default as useDispatchHideNotification } from './useDispatchHideNotification';
+export { default as useUserLoading } from './useUserLoading';
--- src/modules/SignUpForm/hooks/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as useUserLoading } from './useUserLoading';
