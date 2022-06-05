feb56a39f047e0ff9e2a316c47c5f08ab9ed9115
Zack Davis <zack.davis88@hotmail.com>
2022-06-05 15:55:27 -0700

    restructuring NextJS pages folder

---
+++ pages/blueprints/create.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { GetServerSideProps } from 'next';
+import { getAuthToken } from 'src/utils';
+
+function BlueprintCreatePage() {
+  return <div>Blueprint Create Page</div>;
+}
+
+// TODO: this SSR method is a placeholder. It will be replaced once this page starts development.
+export const getServerSideProps: GetServerSideProps = getAuthToken({
+  authRequired: true,
+});
+
+export default BlueprintCreatePage;
