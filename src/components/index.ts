bd09e1c86e3ad307c8f9551e4a6ab1df22025043
Zack Davis <zack.davis88@hotmail.com>
2022-06-18 17:16:58 -0700

    adding SectionHeader component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as PopoverMenu } from './PopoverMenu';
 export { default as Form } from './Form';
 export { default as PasswordTextField } from './PasswordTextField';
 export { default as Dialog } from './Dialog';
+export { default as SectionHeader } from './SectionHeader';
